7ec9192d3642b4faa48aedc39468cee56ccd0c3d
amanpervaiz <aman.pervaiz@gmail.com>
2014-11-03 18:43:08 -0800

    Adding test service

---
+++ AzureCustomerServices/TestService/TestService/DAC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace TestService
+{
+    public static class DAC
+    {
+		//private static string m_endpointAddress = @"http://gpslogger.cloudapp.net/CustomerServices.svc";
+		private static string m_endpointAddress = @"http://localhost:25670/CustomerServices.svc";
+
+		private static GpsSvc.CustomerServicesClient getClient()
+		{
+			GpsSvc.CustomerServicesClient prx = new GpsSvc.CustomerServicesClient();
+			prx.Endpoint.Address = new System.ServiceModel.EndpointAddress(m_endpointAddress);
+			//this sets the timeout of the service call, which should give you enough time
+			//  to finish debugging your service call
+			prx.InnerChannel.OperationTimeout = new TimeSpan(0, 5, 0);
+			return prx;
+		}
+
+		internal static string AddGpsData(string userId, string deviceId, string time, string longitude, string latitude, string altitude)
+		{
+			GpsSvc.CustomerServicesClient prx = getClient();
+			return prx.AddGpsData(userId, deviceId, time, longitude, latitude, altitude);
+		}
+
+		internal static string GetGpsData(string userId, string deviceId, string time, out DataSet gpsData)
+		{
+			GpsSvc.CustomerServicesClient prx = getClient();
+		    return prx.GetGpsData(out gpsData, userId, deviceId, time);
+		}
+
+		internal static string DeleteGpsData(string userId, string deviceId, string time)
+		{
+			GpsSvc.CustomerServicesClient prx = getClient();
+			return prx.DeleteGpsData(userId, deviceId, time);
+		}
+    }
+}
---
+++ AzureCustomerServices/TestService/TestService/MainForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestService
+{
+    public partial class MainForm : Form
+    {
+
+        public DataSet ds_GpsData;
+        public BindingSource bs_GpsData;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string errorMessage = string.Empty;
+            if (ValidateFields())
+            {
+                this.Cursor = Cursors.WaitCursor;
+				errorMessage = DAC.AddGpsData(txtUserId.Text, txtDeviceId.Text, txtTime.Text, txtLongitude.Text, txtLatitude.Text, txtAltitude.Text);
+                this.Cursor = Cursors.Default;
+            }
+            if (errorMessage.Length > 0)
+                MessageBox.Show(errorMessage);
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            string errorMessage = string.Empty;
+            if (ValidateFields())
+            {
+                this.Cursor = Cursors.WaitCursor;
+				errorMessage = DAC.GetGpsData(txtUserId.Text, txtDeviceId.Text, txtTime.Text, out ds_GpsData);
+                this.Cursor = Cursors.Default;
+            }
+            if (errorMessage.Length > 0)
+                MessageBox.Show(errorMessage);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string favoriteMovie = string.Empty;
+            string favoriteLanguage = string.Empty;
+            string errorMessage = string.Empty;
+            if (ValidateFields())
+            {
+                this.Cursor = Cursors.WaitCursor;
+				errorMessage = DAC.DeleteGpsData(txtUserId.Text, txtDeviceId.Text, txtTime.Text);
+                this.Cursor = Cursors.Default;
+            }
+
+        }
+
+        private bool ValidateFields()
+        {
+            bool success = true;
+            txtDeviceId.Text = txtDeviceId.Text.Trim();
+            txtUserId.Text = txtUserId.Text.Trim();
+            txtTime.Text = txtTime.Text.Trim();
+            txtLongitude.Text = txtLongitude.Text.Trim();
+			txtLatitude.Text = txtLatitude.Text.Trim();
+			txtAltitude.Text = txtAltitude.Text.Trim();
+
+            if (txtDeviceId.Text.Length == 0)
+            {
+                success = false;
+                MessageBox.Show("Must specify Device Id.");
+            }
+
+			if (txtUserId.Text.Length == 0)
+			{
+				success = false;
+				MessageBox.Show("Must specify User Id.");
+			}
+
+			if (txtTime.Text.Length == 0)
+			{
+				success = false;
+				MessageBox.Show("Must specify Time.");
+			}
+
+			if (txtLongitude.Text.Length == 0)
+			{
+				success = false;
+				MessageBox.Show("Must specify Longitude.");
+			}
+
+			if (txtLatitude.Text.Length == 0)
+			{
+				success = false;
+				MessageBox.Show("Must specify Latitude.");
+			}
+
+			if (txtAltitude.Text.Length == 0)
+			{
+				success = false;
+				MessageBox.Show("Must specify Altitude.");
+			}
+
+            return success;
+
+        }
+    }
+}
